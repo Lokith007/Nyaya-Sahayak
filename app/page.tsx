a9b405ad40b0b98189df1cd8371389188a384357
Lokith <lokithk93@gmail.com>
2026-01-22 13:06:36 +0530

    code added

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+import NyayaSahayak from "@/components/NyayaSahayak";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-slate-50">
+      <NyayaSahayak />
+    </main>
+  );
+}
